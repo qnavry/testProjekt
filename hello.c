430adba72e29379b59cce01f5be196d62a88a1d6
Daniel Scislowski <scislowski.daniel@gmail.com>
2013-10-20 11:51:01 +0200

    Dodano elementy superpomysłu.

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    printf( "I am alive!  Beware.\n" );
+    printf( "Jestem żywy. Uważaj ;)\n" );
+    printf( "Ale fajnie, że to jest po polsku.\n" );
     getchar();
     return 0;
     //dodatkowy komentarz
+    // następny komentarz, element super pomysłu
 }
